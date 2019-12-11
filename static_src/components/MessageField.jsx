f21bf632091cc25c8c6a9653b19ac964f8323598
Makeev Ivan <makeev@phystech.edu>
2019-12-11 19:22:55 +0300

    Подготовка к Уроку 4

--- static_src/components/MessageField.jsx
+++ static_src/components/MessageField.jsx
@@ -5,67 +5,64 @@ import Message from './Message';
 import '../styles/styles.css';
 
 export default class MessageField extends React.Component {
-   state = {
-       messages: [{ text: "Привет!", sender: 'bot' }, { text: "Как дела?", sender: 'bot' }],
-       input: '',
-   };
+    state = {
+        messages: [{ text: "Привет!", sender: 'bot' }, { text: "Как дела?", sender: 'bot' }],
+        input: '',
+    };
 
-   textInput = React.createRef();
+    componentDidUpdate(prevProps, prevState) {
+        if (prevState.messages.length < this.state.messages.length &&
+            this.state.messages[this.state.messages.length - 1].sender === 'me') {
+            setTimeout(() =>
+                    this.setState({
+                        messages: [ ...this.state.messages, {text: 'Не приставай ко мне, я робот!', sender: 'bot'} ] }),
+                1000);
+        }
+    }
 
-   // Ставим фокус на <TextInput> при монтировании компонента
-   componentDidMount() {
-       this.textInput.current.focus();
-   }
+    handleClick = (message) => {
+        this.sendMessage(message)
+    };
 
-   componentDidUpdate() {
-       if (this.state.messages[this.state.messages.length - 1].sender === 'me') {
-           setTimeout(() =>
-                   this.setState({
-                       messages: [ ...this.state.messages, {text: 'Не приставай ко мне, я робот!', sender: 'bot'} ] }),
-               1000);
-       }
-   }
+    handleChange = (event) => {
+        this.setState({ [event.target.name]: event.target.value });
+    };
 
-   handleSendMessage = () => {
-       this.setState({
-           messages: [ ...this.state.messages, {text: this.state.input, sender: 'me'} ],
-           input: '',
-       });
-   };
+    handleKeyUp = (event, message) => {
+        if (event.keyCode === 13) { // Enter
+            this.sendMessage(message)
+        }
+    };
 
-   handleInput = (e) => {
-       this.setState({ [e.target.name]: e.target.value })
-   };
+    sendMessage = (message) => {
+        this.setState({
+            messages: [ ...this.state.messages, {text: message, sender: 'me'} ],
+            input: '',
+        });
+    };
 
-   handleKeyUp = (e) => {
-       if (e.keyCode === 13) {  // Enter
-           this.handleSendMessage()
-       }
-   };
+    render() {
+        const messageElements = this.state.messages.map((message, index) => (
+            <Message key={ index } text={ message.text } sender={ message.sender } />));
 
-   render() {
-       const messageElements = this.state.messages.map((message, index) => (
-           <Message key={ index } text={ message.text } sender={ message.sender } />));
-
-       return <div className="layout">
-           <div className="message-field">
-               { messageElements }
-               <div style={ { width: '100%', display: 'flex' } }>
-                   <TextField
-                       name="input"
-                       ref={ this.textInput }
-                       fullWidth={ true }
-                       hintText="Введите сообщение"
-                       style={ { fontSize: '22px' } }
-                       onChange={ this.handleInput }
-                       value={ this.state.input }
-                       onKeyUp={ this.handleKeyUp }
-                   />
-                   <FloatingActionButton onClick={ this.handleSendMessage }>
-                       <SendIcon />
-                   </FloatingActionButton>
-               </div>
-           </div>
-       </div>
-   }
+        return [
+            <div className="message-field">
+                { messageElements }
+            </div>,
+            <div style={ { width: '100%', display: 'flex' } }>
+                <TextField
+                    name="input"
+                    fullWidth={ true }
+                    hintText="Введите сообщение"
+                    style={ { fontSize: '22px' } }
+                    onChange={ this.handleChange }
+                    value={ this.state.input }
+                    onKeyUp={ (event) => this.handleKeyUp(event, this.state.input) }
+                />
+                <FloatingActionButton onClick={ () => this.handleClick(this.state.input) }>
+                    <SendIcon />
+                </FloatingActionButton>
+            </div>
+        ]
+    }
 }
--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MessageField from './components/MessageField';
+import Layout from './components/Layout'
 
 ReactDOM.render(
     <MuiThemeProvider>
-        <MessageField/>
+        <Layout />
     </MuiThemeProvider>,
    document.getElementById('root'),
 );
