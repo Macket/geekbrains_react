2ba89941bba619520ce9d9c7dbf7c18cb0af7802
Makeev Ivan <makeev@phystech.edu>
2019-12-18 19:48:50 +0300

    Урок 4. Роутинг в React: разбиваем мессенджер на чаты

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Layout from './components/Layout'
+import { BrowserRouter } from 'react-router-dom';
+import Router from './components/Router'
 
 ReactDOM.render(
-    <MuiThemeProvider>
-        <Layout />
-    </MuiThemeProvider>,
+    <BrowserRouter>
+        <MuiThemeProvider>
+            <Router />
+        </MuiThemeProvider>
+    </BrowserRouter>,
    document.getElementById('root'),
 );
