fd6ff6cf7b44952b078c5ab55f317a8bbbe7c294
Makeev Ivan <makeev@phystech.edu>
2019-12-25 19:45:11 +0300

    Подготовка к Уроку 6

--- static_src/components/MessageField.jsx
+++ static_src/components/MessageField.jsx
@@ -5,6 +5,7 @@ import connect from "react-redux/es/connect/connect";
 import { TextField, FloatingActionButton } from 'material-ui';
 import SendIcon from 'material-ui/svg-icons/content/send';
 import Message from './Message';
+import { sendMessage } from '../actions/messageActions';
 import '../styles/styles.css';
 
 
@@ -20,6 +21,20 @@ class MessageField extends React.Component {
         input: '',
     };
 
+    componentDidUpdate(prevProps, prevState) {
+        if (Object.keys(prevProps.messages).length < Object.keys(this.props.messages).length &&
+            this.props.messages[Object.keys(this.props.messages).length].sender === 'me') {
+            setTimeout(() => this.sendMessage('Не приставай ко мне, я робот!', 'bot'), 1000);
+        }
+    }
+
+    sendMessage = (message, sender) => {
+       const { chatId, messages } = this.props;
+       const messageId = Object.keys(messages).length + 1;
+       console.log(messageId, message, sender, chatId);
+       this.props.sendMessage(messageId, message, sender, chatId);
+    };
+
     handleChange = (event) => {
         this.setState({ [event.target.name]: event.target.value });
     };
@@ -32,7 +47,7 @@ class MessageField extends React.Component {
 
     handleSendMessage = (message, sender) => {
        if (message.length > 0 || sender === 'bot') {
-           this.props.sendMessage(message, sender);
+           this.sendMessage(message, sender);
        }
        if (sender === 'me') {
            this.setState({ input: '' })
@@ -72,10 +87,11 @@ class MessageField extends React.Component {
     }
 }
 
-const mapStateToProps = ({ chatReducer }) => ({
-   chats: chatReducer.chats,
+const mapStateToProps = ({ chatReducer, messageReducer }) => ({
+    chats: chatReducer.chats,
+    messages: messageReducer.messages,
 });
 
-const mapDispatchToProps = dispatch => bindActionCreators({}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({ sendMessage }, dispatch);
 
 export default connect(mapStateToProps, mapDispatchToProps)(MessageField);
